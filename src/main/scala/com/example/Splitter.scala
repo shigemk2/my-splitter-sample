04e31d42df569adef17c01a7a7900c16593e33f5
Michihito Shigemura <m_shigemura@shigemk2.com>
2016-12-28 01:35:19 +0900

    Add Order

--- src/main/scala/com/example/Splitter.scala
+++ src/main/scala/com/example/Splitter.scala
@@ -2,5 +2,13 @@ package com.example
 
 import akka.actor._
 
+case class Order(orderItems: Map[String, OrderItem]) {
+  val grandTotal: Double = orderItems.values.map(_.price).sum
+
+  override def toString = {
+    s"Order(Order Items: $orderItems Totaling: $grandTotal)"
+  }
+}
+
 object SplitterDriver extends CompletableApp(4) {
 }
